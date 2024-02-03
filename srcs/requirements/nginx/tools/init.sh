14ac55a7ac356f0a4a6b86fa3ef13814fd843267
Your Name <you@example.com>
2024-02-03 08:51:13 -0500

    adding labels to each container

--- srcs/requirements/nginx/tools/init.sh
+++ srcs/requirements/nginx/tools/init.sh
@@ -7,6 +7,6 @@ openssl genrsa -out $TLS_CERTIFICATION_PRIVATE_KEY 4096
 openssl req -new -key $TLS_CERTIFICATION_PRIVATE_KEY -out $TLS_CERTIFICATION_SIGNING_REQUEST -subj "/C=CA/ST=ON/L=Toronto/O=Amine Org/CN=Amine"
 
 # self-signed CA certificate
-openssl x509 -req -in $TLS_CERTIFICATION_SIGNING_REQUEST -signkey $TLS_CERTIFICATION_PRIVATE_KEY -out $TLS_CERTIFICATION -days 1080
+openssl x509 -req -in $TLS_CERTIFICATION_SIGNING_REQUEST -signkey $TLS_CERTIFICATION_PRIVATE_KEY -out $TLS_CERTIFICATION -days 365
 
 nginx -g 'daemon off;'
